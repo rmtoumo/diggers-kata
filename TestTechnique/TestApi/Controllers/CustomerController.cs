eadbe7c163b385f4ad88646c240a6911febb556e
Richard.M <rmtoumo-ext@alpheys.com>
2023-03-21 18:16:06 +0100

    remove routes on custom controller

--- TestTechnique/TestApi/Controllers/CustomerController.cs
+++ TestTechnique/TestApi/Controllers/CustomerController.cs
@@ -24,12 +24,6 @@ namespace TestApi.Controllers
             // L'implémentation doit être asynchrone
             // Il ne faut pas retourner l'entité directement, mais créer une méthode d'extension qui renvoie une DTO avec les données demandées
 
-        [HttpGet("{id}")]
-        public IActionResult GetCustomer(int id)
-        {
-            throw new NotImplementedException();
-        }
-
 
 
         // Implémenter une route qui va récupérer la liste des clients [{nom, prénom, email}] ayant acheté un produit avec l'id donné en entré
@@ -37,10 +31,5 @@ namespace TestApi.Controllers
             // Si il n'y a pas de résultats, la route doit renvoyer le résultat MVC approprié
             // L'implémentation doit être asynchrone
             // Il ne faut pas retourner l'entité directement, mais créer une méthode d'extension qui renvoie une DTO avec les données demandées
-        [HttpGet("GetCustomersByProductId/{id}")]
-        public IActionResult GetCustomersByProductId(int id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
